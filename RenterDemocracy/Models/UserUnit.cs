71e1dcc800864a3bbe2477b05e26f8f7800c9d39
Leo Merrill <lm755387@southeast.edu>
2023-11-06 19:28:03 -0600

    Added owner functionality

--- RenterDemocracy/Models/UserUnit.cs
+++ RenterDemocracy/Models/UserUnit.cs
@@ -5,7 +5,7 @@ namespace RenterDemocracy.Models
 {
     public class UserUnit
     {
-        public Guid UserId { get; set; }
+        public string UserId { get; set; } = string.Empty;
         public User User { get; set; } = new User();
         public Guid UnitId { get; set; }
         public Unit Unit { get; set; } = new Unit();
