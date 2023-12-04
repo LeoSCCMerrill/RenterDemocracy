8fbc73cca1f76654c68370d098488f7a2f979441
Leo Merrill <lm755387@southeast.edu>
2023-12-04 10:02:07 -0600

    Remove unused class

--- RenterDemocracy/Models/PostViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace RenterDemocracy.Models
-{
-    public class PostViewModel
-    {
-        public KeyValuePair<Unit, IList<Post>> Posts { get; set; } = new KeyValuePair<Unit, IList<Post>>();
-    }
-}
